cfd11c45655f10dc91d88da9a1094be4e4e5cad6
Eric Simmerman <ericsimmerman@gmail.com>
2026-01-27 19:18:38 -0500

    update icon, purge dist

--- dist/nodes/QualyticsTrigger/QualyticsTrigger.node.d.ts
+++
@@ -1,5 +0,0 @@
-import type { IWebhookFunctions, IWebhookResponseData, INodeType, INodeTypeDescription } from 'n8n-workflow';
-export declare class QualyticsTrigger implements INodeType {
-    description: INodeTypeDescription;
-    webhook(this: IWebhookFunctions): Promise<IWebhookResponseData>;
-}
